5cbc84c655ef248c7fb6e553009bb71833c5602b
m0r15 <serenkovav@gmail.com>
2023-08-02 15:54:50 +0300

    fix function
    change function name from main
    change CMake version

--- main.cpp
+++ main.cpp
@@ -4,71 +4,24 @@
 #include <QDateTime>
 #include <QDebug>
 
-void getBootStamp();
+QString getBootTime();
 
 int main(int argc, char *argv[]) {
 
     QCoreApplication a(argc, argv);
     qDebug() << "Hello World";
 
-    getBootStamp();
+    getBootTime();
 
     return QCoreApplication::exec();
 }
 
-void getBootStamp()
+QString getBootTime()
 {
     struct utmpx *utx;
     while ((utx = getutxent()) != nullptr) {
         if (BOOT_TIME == utx->ut_type) {
-            qDebug() << "Boot time" << QDateTime::fromTime_t(utx->ut_tv.tv_sec);
+            qDebug() << "Boot time" << QDateTime::fromTime_t(utx->ut_tv.tv_sec).toString("yyyy.MM.dd HH:mm:sss");
         }
     }
 }
-
-//static void
-//process_utmp(void)
-//{
-//    struct utmpx *utx;
-//
-//    while ((utx = getutxent()) != nullptr) {
-//#ifdef __APPLE__
-//        if (aflag) {
-//            row(utx);
-//        }
-//        else if (!bflag && utx->ut_type == USER_PROCESS) {
-//            if (ttystat(utx->ut_line) == 0) {
-//                if (!rflag && !lflag && !dflag) {
-//                    row(utx);
-//                }
-//            }
-//        }
-//        else if (bflag && utx->ut_type == BOOT_TIME) {
-//#ifndef __APPLE__
-//            if (ttystat(utx->ut_line) == 0) {
-//#endif /* __APPLE__ */
-//
-//            row(utx);
-//#ifndef __APPLE__
-//            }
-//#endif /* __APPLE__ */
-//        }
-//        else if (dflag && utx->ut_type == DEAD_PROCESS) {
-//            row(utx);
-//        }
-//        else if (lflag && utx->ut_type == LOGIN_PROCESS) {
-//            row(utx);
-//        }
-//#else
-//        if (((aflag || !bflag) && utx->ut_type == USER_PROCESS) ||
-//		    (bflag && utx->ut_type == BOOT_TIME))
-//			if (ttystat(utx->ut_line) == 0)
-//				row(utx);
-//#endif
-//    }
-//#ifdef __APPLE__
-//    if (rflag) {
-//        printf("   .       run-level 3\n");
-//    }
-//#endif
-//}
